52870474a9d4f449efabd1f0ead750e0d6b3ab82
signature2017 <joel.cabrera@envirostudies.org>
2017-05-19 13:25:18 +0000

    started styling the site minor Changes

--- script.js
+++ script.js
@@ -7,15 +7,14 @@ $(document).ready(function() {
             $(".contain").append('<div class="event"> \
             <div class="name">' + json[i].name +'</div> \
             <div class="ls"> \
-                <div class="location"></div> \
-                <div class="summary"></div> \
+                <div class="location">'+ json[i].location +'</div> \
+                <div class="summary">'+ json[i].Summary +'</div> \
             </div> \
             <div class="pd"> \
-                <div class="picture"></div> \
-                 <div class="date"></div> \
+                <div class="picture"><img src="'+ json[i].picture +'" /></div> \
+                 <div class="date">'+ json[i].date +'</div> \
              </div> \
         </div>');
         }
-        // $(".picture").append("<img src="+ json[0].picture +" />")
     });
 })
